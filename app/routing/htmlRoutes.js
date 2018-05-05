b824507cbb3bf49235e455ad0b41533ae82f16d6
Christopher Etges <c.etges86@gmail.com>
2018-05-05 10:02:31 -0600

    frontend work

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -8,6 +8,16 @@ router.get("/", function(req,res){
     res.sendFile(path.join(__dirname,"../public/home.html"))
 })
 
+router.get("/", function(req, res){
+    res.set({'content-type': 'text/css'});
+    res.sendFile(path.join(__dirname,"../public/css/style.css"))
+})
+
+router.get("/", function(req, res){
+    // res.set({"content-type": "text/javascript"});
+    res.sendFile(path.join(__dirname,"app/public/js/script.js"))
+})
+
 router.get("/survey", function(req,res){
     res.sendFile(path.join(__dirname,"../public/survey.html"))
 })
