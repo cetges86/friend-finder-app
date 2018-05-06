6a2598a0e58197450f7faf22b14887307221bd06
Christopher Etges <c.etges86@gmail.com>
2018-05-06 13:15:13 -0600

    finish Logic

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,8 +1,25 @@
 
 
 module.exports = surveyResults = [{
-    "name":"Chris",
-    "image":"https://avatars2.githubusercontent.com/u/32533644?s=460&v=4",
-    "scores":[5,5,4,1,3,4,5,3,4,4]
+    "name": "Chris",
+    "image": "https://avatars2.githubusercontent.com/u/32533644?s=460&v=4",
+    "scores": [5, 5, 4, 1, 3, 4, 5, 3, 4, 4]
+},
+{
+    "name": "Diana",
+    "image": "https://media.licdn.com/dms/image/C4E03AQFudpDvAx7NVA/profile-displayphoto-shrink_800_800/0?e=1531353600&v=beta&t=IAZUTmY2xdV0NR_sG4CvHT5ADHOAL9JCllwDQCbiZu8",
+    "scores": [4, 4, 3, 4, 2, 3, 4, 4, 2, 2]
+}, {
+    "name": "Eric Cartman",
+    "image": "http://3.images.southparkstudios.com/blogs/southparkstudios.com/files/2014/06/0602-cartman-phone1.jpg?quality=0.8",
+    "scores": [3, 2, 3, 1, 4, 5, 4, 1, 2, 1]
+}, {
+    "name": "Ned Flanders",
+    "image": "http://www.simpsonsforever.com/images/episodes/characters/15.jpg",
+    "scores": [1, 3, 5, 1, 5, 1, 1, 2, 5, 3]
+}, {
+    "name": "Groundskeeper Willie",
+    "image": "https://vignette.wikia.nocookie.net/simpsons/images/4/47/Hokuto_no_willie.jpg/revision/latest?cb=20110215003415",
+    "scores": [1, 3, 2, 5, 1, 2, 3, 5, 4, 4]
 }];
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,23 +1,59 @@
 const server = require('../../server')
-const express = require("express");
-const bodyParser = require("body-parser")
-const router = express.Router();
 const users = require('../data/friends')
+const express = require("express");
+const router = express.Router();
+const bodyParser = require("body-parser")
 
-router.use(bodyParser.urlencoded({extended:true}));
+router.use(bodyParser.urlencoded({ extended: true }));
 router.use(bodyParser.json());
 
-router.get("/api/friends", function(req,res){
+router.get("/api/friends", function (req, res) {
     return res.json(surveyResults);
 });
 
-router.post("/api/friends", function(req,res){
-    
-    var newFriend = req.body;
-    surveyResults.push(newFriend);
-    res.json(newFriend);
+router.post("/api/friends", function (req, res) {
 
+    let newFriend = req.body;
+    console.log(newFriend);
+
+    let answers = newFriend.scores;
+
+    let friendName = "";
+    let friendImg = "";
+    let total = 500;
     
+    
+    for (let i = 0; i < surveyResults.length; i++) {
+        let diff = 0;
+        let difference = [];
+        
+        for (let j = 0; j < answers.length; j++) {
+
+            let diff = Math.abs(answers[j] - surveyResults[i].scores[j]);
+            difference.push(diff);
+
+        }
+        console.log(difference);
+        let sum = difference.reduce(function (acc, val) { return acc + val; });
+        console.log("sum is " + sum)
+
+        if (sum < total) {
+            total = sum;
+            sum = 0;
+            friendName = surveyResults[i].name;
+            friendImg = surveyResults[i].image;
+        } else {
+            sum = 0;
+        }
+    }
+
+    surveyResults.push(newFriend);
+    res.json({
+        friendName: friendName,
+        friendImg: friendImg
+    });
+
+
 
 });
 
